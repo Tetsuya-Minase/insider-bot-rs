1b5719fa34b28d1652dc4c4890da6f9c5be595f9
Tetsuya Minase <tetsuya.minasen@gmail.com>
2022-04-26 23:55:11 +0900

    create discord bot (#1)
    * feat: add sample
    
    * feat: get channel member.
    
    * feat: create player.rs
    
    * feat: create get_theme
    
    * feat: hand out role
    
    * doc: add document comment.
    
    * feat: use mention.
    
    * Revert "feat: use mention."
    
    This reverts commit 9f4891f97e0ece8b8d2f931bbb6cf3024da8a9e1.
    
    * Revert "Revert "feat: use mention.""
    
    This reverts commit 5caa1d1be9e9ccd6c4cdce4c7035a166561a3eae.
    
    * build: update library.
    
    * feat: send role and theme via DM.
    
    * refactor: remove unused method and fix method name.
    
    * refactor: remove unused imports.
    
    * docs: add comment
    
    * feat: fix error handling

---
+++ src/insider_game.rs
@@ -0,0 +1,46 @@
+use rand::seq::SliceRandom;
+use serenity::model::user::User;
+
+/// get theme
+pub fn get_theme() -> Option<String> {
+    let mut theme_list = vec!["山", "学校", "算数", "ドラゴン", "テスト"];
+    let mut rng = rand::thread_rng();
+    theme_list.shuffle(&mut rng);
+    let option_target = theme_list.get(0);
+    if let Some(target) = option_target {
+        return Some(target.to_string());
+    };
+    None
+}
+
+/// player and role struct
+pub struct PlayerRole {
+    /// player name
+    pub player_name: User,
+    /// player role
+    pub role: String
+}
+
+/// hand out roles.
+/// # Arguments
+/// * `players` - player name list
+pub fn hand_out_role(players: Vec<User>) -> Vec<PlayerRole> {
+    let mut roles = vec!["マスター", "インサイダー", "市民", "市民"];
+    if players.len() > 4 {
+        let append_player_count = players.len() - 1;
+        for _count in 1..append_player_count {
+            roles.push("市民");
+        }
+    }
+    let mut rng = rand::thread_rng();
+    roles.shuffle(&mut rng);
+
+    let mut player_role_list: Vec<PlayerRole> = vec![];
+    for player in players {
+        let option_role = roles.pop();
+        if let Some(role) = option_role {
+            player_role_list.push(PlayerRole {player_name: player, role: role.to_string()});
+        }
+    }
+    player_role_list
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
-fn main() {
-    println!("Hello, world!");
+mod insider_game;
+
+use std::{
+    collections::HashSet,
+    env,
+};
+
+use serenity::{async_trait, framework::standard::{
+    Args,
+    CommandGroup,
+    CommandResult,
+    help_commands,
+    HelpOptions,
+    macros::{command, group, help},
+}, http::Http, model::{channel::Message, gateway::Ready, id::UserId}, prelude::*};
+use serenity::framework::StandardFramework;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[group]
+#[summary = "General"]
+#[description = "General commands."]
+#[commands(play_insider_game)]
+struct General;
+
+#[command]
+#[aliases("insider")]
+async fn play_insider_game(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    let players = msg.mentions.to_vec();
+    let option_theme = insider_game::get_theme();
+    let theme = match option_theme {
+        Some(theme) => theme,
+        None => return Ok(())
+    };
+    let player_role_list = insider_game::hand_out_role(players);
+    for player_role in player_role_list {
+        let message = match &*player_role.role {
+            "マスター" | "インサイダー" => format!("Hello {}.\nYour role is {}.\nThe theme is {}.", player_role.player_name, player_role.role, theme),
+            "市民" => format!("Hello {}.\nYour role is {}.", player_role.player_name, player_role.role),
+            _ => "".to_string()
+        };
+        let result = player_role.player_name.direct_message(&ctx, |m| m.content(message)).await;
+        if let Err(_result) = result {
+            msg.channel_id.say(&ctx.http, "Error!! see logs.").await?;
+        }
+    }
+    msg.channel_id.say(&ctx.http, "配ったよ").await?;
+    Ok(())
+}
+
+#[help]
+#[individual_command_tip = "this is help command."]
+async fn my_help(
+    context: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let http = Http::new(&token);
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c
+            .prefix("~")
+            .on_mention(Some(bot_id))
+            .owners(owners)
+        )
+        .help(&MY_HELP)
+        .group(&GENERAL_GROUP);
+    let mut client =
+        Client::builder(&token, GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILD_PRESENCES)
+            .event_handler(Handler)
+            .framework(framework)
+            .await.expect("Err creating client");
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
